4e13b5b3dc6e6f8828d9050bfc8fd10de7693689
sheaksadi <sheaksadi123456@gmail.com>
2024-01-03 20:21:37 +0600

    added discord hook

--- app.vue
+++ app.vue
@@ -26,14 +26,31 @@ const onHover = (e) => {
   top: ${y}px;
   left: ${x}px;
 `
+  timesTriedToClickNo.value++
 }
 const onNoClick = (e) => {
   onHover(e)
 }
+
+function discord_message(webHookURL, message) {
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", webHookURL, true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send(JSON.stringify({
+    'content': message,
+    'username':'Frieren',
+  }));
+}
 const onYesClick = (e) => {
   gifUrl.value = "https://media.giphy.com/media/vnh5zLtbpFbhe/giphy.gif"
   yesClicked.value = true
+
+  discord_message("https://discord.com/api/webhooks/1192107935011188747/M-iq8cOdreqL0ON3mMMZDTDWA6UuKoG6meJgKmxDv5JsgxFrIjpb85MgpbmveEhYwsCO",
+  `you got a fwend yay! :)\nbefore Tried no button ${timesTriedToClickNo.value} times`
+  )
+
 }
+let timesTriedToClickNo = ref(0)
 let noButtonStyle = ref("")
 let gifUrl = ref("assets/frieren-sousou-no-frieren.gif")
 let yesClicked = ref(false)
@@ -47,8 +64,8 @@ let noButton = ref(null)
     <div class="h-[28rem] w-[20rem]  flex flex-col">
 
       <div class="h-full w-full basis-5/6 ">
-        <img v-if="!yesClicked" class="h-full w-full rounded" src="assets/frieren-sousou-no-frieren.gif" alt="this slowpoke moves"  width="250" />
-        <img v-if="yesClicked" class="h-full w-full rounded" src="assets/frieren-anime.gif" alt="this slowpoke moves"  width="250" />
+        <img v-if="!yesClicked" class="h-full w-full rounded" src="assets/frieren-sousou-no-frieren.gif" alt="a cute elf :)"  width="250" />
+        <img v-if="yesClicked" class="h-full w-full rounded" src="assets/frieren-anime.gif" alt="a cute elf :)"  width="250" />
       </div>
       <div v-if="!yesClicked" class="h-full w-full basis-1/6  flex p-2 gap-2">
         <div class="w-full h-full">
